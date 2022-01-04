7681ff0bd604707e6a548eb81e4d07024b2dbe35
YashAgrawal <71179045+yash2032@users.noreply.github.com>
2022-01-04 22:31:19 +0530

    Create 8. Total Decoding Messages.cpp

---
+++ Goldman Sachs/8. Total Decoding Messages.cpp
@@ -0,0 +1,25 @@
+int CountWays(string digits){
+		    
+		    int n=digits.length();
+		    int count[n+1]; 
+            count[0] = 1;
+            count[1] = 1;
+    
+            if(digits[0]=='0')  
+             return 0;
+             
+            for (int i = 2; i <= n; i++)
+            {
+                count[i] = 0;
+        
+                if (digits[i-1] > '0')
+                    count[i] = count[i-1];
+        
+               
+                if (digits[i-2] == '1' || 
+                      (digits[i-2] == '2' && digits[i-1] < '7') )
+                    count[i] =(count[i]+ count[i-2])%(1000000007);
+            }
+            return count[n]%(1000000007);
+		    
+		}
