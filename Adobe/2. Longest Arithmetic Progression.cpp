56f2712557a226259e86352b173e96f102e16c8a
YashAgrawal <71179045+yash2032@users.noreply.github.com>
2022-01-16 18:02:38 +0530

    Update 2. Longest Arithmetic Progression.cpp

--- Adobe/2. Longest Arithmetic Progression.cpp
+++ Adobe/2. Longest Arithmetic Progression.cpp
@@ -1,21 +1,30 @@
 // https://practice.geeksforgeeks.org/problems/longest-arithmetic-progression1019/1/#
 
-int lengthOfLongestAP(int nums[], int n) {
-         int ans = 0;
-           if(n == 1) return 1;
-    
-            vector<vector<int>>dp(n, vector<int>(10001));
-            
-            for(int i = 0; i < n; i++) {
-                for(int j = i + 1; j < n; j++) {
-    
-                    int d = nums[j] - nums[i];
-    
-                    dp[j][d] = max(dp[i][d] + 1, 2);
-    
-                    ans = max(ans, dp[j][d]);
-                }
-            }
-    
-            return ans;
+int lengthOfLongestAP(int arr[], int n) {
+         if(n==1) return 1;
+       
+         if(n==2) return 2;
+       
+           vector<unordered_map<int,int>> dp(n);
+           int ans = 0;
+           for(int i=1;i<n;i++)
+           {
+               
+               for(int j=0;j<i;j++)
+               {
+                   int diff = arr[i] - arr[j];
+                   
+                   if(dp[j].find(diff)==dp[j].end())
+                   {
+                       dp[i][diff] = 2;
+                   }else
+                   {
+                       dp[i][diff] = dp[j][diff]+1;
+                   }
+                   ans = max(ans,dp[i][diff]);
+                   
+               }
+           }
+          
+           return ans;
     }
