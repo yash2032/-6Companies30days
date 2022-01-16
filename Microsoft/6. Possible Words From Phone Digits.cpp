98f1a5196dfa661de5956c2224517a6d588520d1
YashAgrawal <71179045+yash2032@users.noreply.github.com>
2022-01-16 15:29:43 +0530

    Create 6. Possible Words From Phone Digits.cpp

---
+++ Microsoft/6. Possible Words From Phone Digits.cpp
@@ -0,0 +1,39 @@
+// https://practice.geeksforgeeks.org/problems/possible-words-from-phone-digits-1587115620/1/#
+
+class Solution
+{
+    public:
+    //Function to find list of all words possible by pressing given numbers.
+    void print(int i,int arr[],vector<vector<char>>&dict,int n,vector<string>&ans,string str){
+        
+        if(i >= n){
+            ans.push_back(str);
+            return;
+        }
+        
+        int currSize = dict[arr[i]-2].size();
+        
+        for(int j=0;j<currSize;j++){
+            print(i+1,arr,dict,n,ans,str + dict[arr[i]-2][j]);
+        }
+    }
+    
+    vector<string> possibleWords(int arr[], int n){
+        vector<string> ans;
+        vector<vector<char>> dict;
+        dict.push_back({'a','b','c'});
+        dict.push_back({'d','e','f'});
+        dict.push_back({'g','h','i'});
+        dict.push_back({'j','k','l'});
+        dict.push_back({'m','n','o'});
+        dict.push_back({'p','q','r','s'});
+        dict.push_back({'t','u','v'});
+        dict.push_back({'w','x','y','z'});
+        
+        print(0,arr,dict,n,ans,"");
+        
+        return ans;
+    }
+};
+
+
