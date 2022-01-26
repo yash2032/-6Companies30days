5f3ac764712ee2b0b3b84d7714563b64db1d1bd2
YashAgrawal <71179045+yash2032@users.noreply.github.com>
2022-01-26 23:16:14 +0530

    Create 2. Stone Game.cpp

---
+++ Walmart/2. Stone Game.cpp
@@ -0,0 +1,11 @@
+// https://leetcode.com/problems/stone-game/submissions/
+
+class Solution {
+public:
+    bool stoneGame(vector<int>& piles) {
+        
+        return true;
+        
+        
+    }
+};
